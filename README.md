66b4a0408698fd1dad4c402a3b2eb8e8a5190af7
wonjinsin <wonjinsin@wonjinui-MacBookPro.local>
2020-04-25 18:27:13 +0900

    add read me && modify index

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # apple_secret_generator
-generate apple client secret simple
+
+Simple Apple Secret key generator for javascript
+
+## Getting Started
+
+### Installing
+
+```
+npm instrall apple_secret_generator
+```
+
+## Usage
+
+```
+const aps = require("apple_secret_generator");
+
+const token = aps.generate({
+  keyFileLocation: "./AppleLoginAuthKey.p8", // put generated p8 file directory
+  expires: 180,
+  team_id: process.env.TEAM_ID, // App ID Prefix's TEAM ID
+  identifier: process.env.IDENTIFIER, // Services IDs IDNETIFIER
+  key_id: process.env.KEY_ID, // Key ID
+});
+
+```
+
+## Built With
+
+* [node-jsonwebtoken](https://github.com/auth0/node-jsonwebtoken) - jwt generator
+
+## Authors
+
+* **wonjinsin** - https://github.com/wonjinsin/apple_secret_generator
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
--- index.js
+++ index.js
@@ -1,19 +1,26 @@
 const jwt = require("jsonwebtoken");
 const fs = require("fs");
 
-export default apple_secret_generator = function(file, expires, team_id, identifier, key_id){
-
-    const privateKey = fs.readFileSync(file); // put p8 file directory
+const apple_secret_generator = {
+  generate: function ({
+    keyFileLocation,
+    expires = "180",
+    team_id,
+    identifier,
+    key_id,
+  }) {
+    const privateKey = fs.readFileSync(keyFileLocation); // put p8 file directory
     const token = jwt.sign({}, privateKey, {
-    algorithm: "ES256",
-    expiresIn: expires + " days",
-    audience: "https://appleid.apple.com",
-    issuer: team_id, // App ID Prefix's TEAM ID
-    subject: identifier, // Services IDs IDNETIFIER
-    keyid: key_id // Key ID
+      algorithm: "ES256",
+      expiresIn: expires + " days",
+      audience: "https://appleid.apple.com",
+      issuer: team_id,
+      subject: identifier,
+      keyid: key_id,
     });
 
     return token;
+  },
+};
 
-}
-
+module.exports = apple_secret_generator;
